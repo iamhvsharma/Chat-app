229c0b09da1ef193b8e2be0070190690398e6da2
Harshvardhan Sharma <itshvsharma@gmail.com>
2025-06-06 13:41:17 +0530

    feat: implement TextInput component with onChange handler and update Home page to use it

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,6 +1,13 @@
+"use client";
+
 import { TextInput } from "@repo/ui/text-input";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function Home() {
+  const [value, setValue] = useState("");
+  const router = useRouter();
+
   return (
     <div
       style={{
@@ -10,12 +17,25 @@ export default function Home() {
         padding: 250,
       }}
     >
-      <div style={{
-        display: "flex",
-        gap: 10
-      }}>
-        <TextInput placeholder="RoomId" />
-        <button>Join</button>
+      <div
+        style={{
+          display: "flex",
+          gap: 10,
+        }}
+      >
+        <TextInput
+          placeholder="RoomId"
+          onChange={(e) => {
+            setValue(e.target.value);
+          }}
+        />
+        <button
+          onClick={() => {
+            router.push(`/chat/${value}`);
+          }}
+        >
+          Join
+        </button>
       </div>
     </div>
   );
--- packages/ui/src/text-input.tsx
+++ packages/ui/src/text-input.tsx
@@ -1,12 +1,17 @@
+'use client';
+
+import { ChangeEvent } from "react";
+
 interface PropType {
     placeholder: string;
+    onChange: (e: ChangeEvent<HTMLInputElement>) => void;
 }
 
 
-export function TextInput({ placeholder }: PropType) {
+export function TextInput({ placeholder, onChange }: PropType) {
     return (
         <div>
-            <input type="text" placeholder={placeholder} />
+            <input type="text" onChange={onChange} placeholder={placeholder} />
         </div>
     )
 }
